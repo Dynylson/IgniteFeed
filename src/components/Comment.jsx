fd6811323638a2bb1ad555750c95d3eec1515886
Dynylson <dynylsonjr@gmail.com>
2023-01-09 13:21:45 -0300

    Componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://avatars.githubusercontent.com/u/94336147?v=4' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Dynylson Junior</strong>
+              <time
+                title='9 de Janeiro às 11:26'
+                dateTime='2023-01-09 11:26:31'
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
