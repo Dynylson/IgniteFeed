34b9425c0090f08746db725a9a9dec7beb751251
Dynylson <dynylsonjr@gmail.com>
2023-01-08 21:35:31 -0300

    Componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+
+import styles from "./App.module.css";
 
 import "./global.css";
 
@@ -7,11 +10,17 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author='Diego Fernandes'
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sequi consectetur amet repellat neque vitae perspiciatis. Corporis, amet obcaecati ipsa, ea eos eveniet repellat omnis dolorum commodi, molestias sint magni?'
-      />
-      <Post author='Gabriel Buzzi' content='Um novo post muito legal' />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Diego Fernandes'
+            content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis velit porro quas, perferendis dolorum sit quisquam! Ad, error et dolore expedita corrupti molestiae deserunt debitis sit soluta odit fugiat possimus.'
+          />
+          <Post author='Gabriel Buzzi' content='Um novo post muito legal' />
+        </main>
+      </div>
     </div>
   );
 }
